5e5894add1a3f68a69dec9f58d576b6bea74143d
ZixUUUs <161074193+ZixUUUs@users.noreply.github.com>
2025-05-15 23:35:16 +0100

    Finishing models for our albums js to rely to our controllers

--- models/Albums.js
+++ models/Albums.js
@@ -1,11 +1,27 @@
 const myClient = require("../Data-base/db");
-myClient.connect();
-myClient.query(`SELECT * FROM radiohead`, (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-    myClient.end();
-  } else {
-    console.log(err);
-    myClient.end();
+
+const getAlbums = async () => {
+  try {
+    let myRequest = await myClient.query(`SELECT * FROM radiohead`);
+    return myRequest.rows;
+  } catch (error) {
+    console.log(error);
+    console.error("couldn't get the Table radiohead");
+    return null;
   }
-});
+};
+const addAlbums = async (title, picture, sumTracks) => {
+  try {
+    let insertNew = await myClient.query(
+      `INSERT INTO radiohead (title, picture, sumTracks) VALUES ($1,$2,$3)`,
+      [title, picture, sumTracks]
+    );
+    console.log("added succesfuly");
+    return insertNew;
+  } catch (error) {
+    console.log(error);
+    console.error(`there's was a probleme in your insertion`);
+    return null;
+  }
+};
+module.exports = { getAlbums, addAlbums };
